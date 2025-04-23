59c655887448e6f53551ca31d67fe3bdf12c57e0
ISTKHAR-king <istkharalam225@gmail.com>
2025-04-23 08:24:57 +0530

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -17,7 +17,7 @@ from SONALI.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from ISTKHAR_MUSIC.utils.decorators.language import LanguageStart
+from SONALI.utils.decorators.language import LanguageStart
 from SONALI.utils.formatters import get_readable_time
 from SONALI.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
