f73c2f5d556cc0ced0187f4a864e8326baccab94
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-05-21 10:48:35 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -27,7 +27,7 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/Avengersbots/471"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/All_super_music/22"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
