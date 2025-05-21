c2ad2d79cc57bd00c1e90db8ca9c4bbae1e04a02
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-05-21 10:19:57 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="|| ˹𖤍 𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗚𝗥𝗢𝗨𝗣𖤍˼ ||",
-            url=f"https://t.me/ClassyMusicRobot?startgroup=true",
+            url=f"https://t.me/MAHI_X_MUSIC_BOT?startgroup=true",
         ),
     ],
 ]
