86cb758cd5842baa57836c8b255f8d16ed63dd82
ISTKHAR-king <istkharalam225@gmail.com>
2025-05-22 23:33:24 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -27,7 +27,7 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/BabyXMusicRobot/817"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/Annusongbot/817"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
